5d56e1d4348fba752d450ce66fb499b7cf79f4b9
Siddhant Pal <palsiddhant3@gmail.com>
2025-06-20 22:53:17 +0530

    fix all register controller

--- Youtube clone/src/controllers/user.controller.js
+++ Youtube clone/src/controllers/user.controller.js
@@ -19,7 +19,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
 
     const { fullName , username, email, password } = req.body;
-    console.log("email: ", email);
+    // console.log("email: ", email);
 
     if (
         [fullName, email, username, password].some((field) =>
@@ -28,7 +28,7 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new ApiError(400, "Please fill in all fields");
     }
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{ username }, { email }]
     })
 
@@ -37,17 +37,22 @@ const registerUser = asyncHandler(async (req, res) => {
     }
     
     const avatarLocalPath =  req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    //const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length >0){
+        coverImageLocalPath = req.files.coverImage[0].path;
+    }
 
     if (!avatarLocalPath) {
-        throw new ApiError(400, "Please upload an avatar");
+        throw new ApiError(400, "Avatar File is required");
     }
 
     const avatar = await uploadOnCloudinary(avatarLocalPath)
     const coverImage = await uploadOnCloudinary(coverImageLocalPath)
 
     if (!avatar) {
-        throw new ApiError(400, "Please upload an avatar");
+        throw new ApiError(400, "Please upload an avatar, required field");
     }
 
 
@@ -60,7 +65,7 @@ const registerUser = asyncHandler(async (req, res) => {
         username: username.toLowerCase()
     })
 
-    const createdUser = await User.findbyId(user._id).select(
+    const createdUser = await User.findById(user._id).select(
         "-password -refreshToken"
     )
 
--- Youtube clone/src/utils/cloudinary.js
+++ Youtube clone/src/utils/cloudinary.js
@@ -1,6 +1,12 @@
 import {v2 as cloudinary} from "cloudinary";
 import fs from "fs";
 
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
+
 const uploadOnCloudinary = async (localfilepath) => {
     try {
         if (!localfilepath) return null
@@ -9,8 +15,8 @@ const uploadOnCloudinary = async (localfilepath) => {
             resource_type: "auto",
         })
         //File has been uploaded successfully
-        console.log("file is uploaded on cloudinary",
-        response.url);
+        // console.log("file is uploaded on cloudinary",response.url);
+        fs.unlinkSync(localfilepath)
         return response;
         
     } catch (error) {

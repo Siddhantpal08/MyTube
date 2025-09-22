e338c7d0392640d26b4f110761212135e43ae39d
Siddhant Pal <palsiddhant3@gmail.com>
2025-09-23 02:26:39 +0530

    Capital

--- Frontend/src/components/SubscriptionsPage.jsx
+++ Frontend/src/components/SubscriptionsPage.jsx
@@ -1,6 +1,6 @@
 // src/components/SubscriptionsPage.jsx
 import React, { useState, useEffect } from 'react';
-import { useAuth } from '../context/AuthContext';
+import { useAuth } from '../Context/AuthContext';
 import axiosClient from '../Api/axiosClient';
 import { Link } from 'react-router-dom';
 import SkeletonCard from './SkeletonCard'; // Reusing our skeleton loader

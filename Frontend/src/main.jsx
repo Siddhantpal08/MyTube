c059c2295dddbd2f2a797b340d00f46900f26b7f
Siddhant Pal <palsiddhant3@gmail.com>
2025-09-23 02:24:58 +0530

    Capital

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { AuthProvider } from './Context/AuthContext.jsx';
-import router from './router.jsx';
+import router from './Router.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(

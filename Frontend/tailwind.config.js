e67b0650b1df027c1d312b323d295e5269c5ef40
Siddhant Pal <palsiddhant3@gmail.com>
2025-09-27 02:44:59 +0530

    fix: update dependencies and adjust Vercel configuration for backend routing

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  // This array tells Tailwind to scan all .jsx files in your src folder
   content: [
     "./Frontend/index.html",
     "./Frontend/src/**/*.{js,ts,jsx,tsx}",

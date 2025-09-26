4bf88da8fc2f8583416ae8a3b90c1bb313ebe2b5
Siddhant Pal <palsiddhant3@gmail.com>
2025-09-27 02:33:31 +0530

    fix: update Tailwind CSS configuration paths and add react-hot-toast dependency

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -2,8 +2,8 @@
 export default {
   // This array tells Tailwind to scan all .jsx files in your src folder
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./Frontend/index.html",
+    "./Frontend/src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},

23466efa96ff871f03bc684e5b8c1920adb236aa
Siddhant Pal <palsiddhant3@gmail.com>
2025-09-27 02:58:44 +0530

    fix: update Tailwind CSS configuration to scan .jsx files and adjust Vercel configuration for backend and frontend

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  // This array tells Tailwind to scan all .jsx files in your src folder
   content: [
-    "./Frontend/index.html",
-    "./Frontend/src/**/*.{js,ts,jsx,tsx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},

a8cd13218f08cecd835c1a752977e1afdd2df5da
Siddhant Pal <palsiddhant3@gmail.com>
2025-10-07 02:40:22 +0530

    Enhance Register component by adding useState for improved state management

--- Frontend/src/components/Register.jsx
+++ Frontend/src/components/Register.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { useAuth } from '../Context/AuthContext';
 import axiosClient from '../Api/axiosClient';

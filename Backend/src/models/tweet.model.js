d566fd2ad10acc8cb3e8cd66161c0bda3851cdbd
Siddhant Pal <palsiddhant3@gmail.com>
2025-10-07 00:48:53 +0530

    Refactor tweet controller for improved functionality and readability; add pagination support and enhance error handling

--- Backend/src/models/tweet.model.js
+++ Backend/src/models/tweet.model.js
@@ -1,5 +1,5 @@
 import mongoose, {Schema} from "mongoose";
-
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const tweetSchema = new Schema(
     {
@@ -16,5 +16,6 @@ const tweetSchema = new Schema(
         timestamps: true
     }
 )
+tweetSchema.plugin(mongooseAggregatePaginate);
 
 export const Tweet = mongoose.model("Tweet", tweetSchema);

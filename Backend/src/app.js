159677dfe827a81f29014dd9a2400076ee65ab9a
Siddhant Pal <palsiddhant3@gmail.com>
2025-10-09 14:34:50 +0530

    log check

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,6 +4,11 @@ import cookieParser from 'cookie-parser';
 
 const app = express();
 
+app.use((req, res, next) => {
+    console.log(`Request Received: ${req.method} ${req.originalUrl}`);
+    next();
+});
+
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true

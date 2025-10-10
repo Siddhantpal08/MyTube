0ae6fe5e88cc5c8c93fa219f68eaae35d0a22e6b
Siddhant Pal <palsiddhant3@gmail.com>
2025-10-10 15:21:58 +0530

    `Added useCallback to import list in CommunityPage.jsx`

--- Frontend/src/components/CommunityPage.jsx
+++ Frontend/src/components/CommunityPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import axiosClient from '../Api/axiosClient'; 
 import { useAuth } from '../Context/AuthContext';
